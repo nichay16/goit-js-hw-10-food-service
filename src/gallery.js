63fc16c9ba34390e5c943692ae40cdf78c9d9093
Evgeny Nichay <nichaj16@gmail.com>
2021-05-07 12:27:24 +0300

    hw3

--- src/gallery.js
+++
@@ -1,7 +0,0 @@
-import gallery from './menu.json';
-import galleryCardsTempl from "./templates/gallery.hbs";
-console.log(galleryCardsTempl);
-
-const menuEl = document.querySelector('.js-menu');
-
-menuEl.insertAdjacentHTML('beforeend', galleryCardsTempl(gallery));
